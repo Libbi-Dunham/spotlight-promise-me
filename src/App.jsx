e7a19f81fd2453149296fb772cc2052bc755cc02
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-25 11:56:27 -0800

    added two more

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { getCharacters } from './services/promise-me';
+import {
+  getCharacters,
+  fetchCharacters,
+  getStatus,
+  fetchStatus,
+} from './services/promise-me';
 import { useEffect } from 'react';
 
 export default function App() {
   useEffect(() => {
+    fetchCharacters();
+  });
+
+  useEffect(() => {
     getCharacters();
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const charactersData = await getStatus();
+      getStatus(charactersData);
+    };
+    fetchData();
+  });
+
+  useEffect(() => {
+    fetchStatus();
   });
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,5 +1,5 @@
 //   TODO: write a fetch request to an api url using .then()
-export const getCharacters = () => {
+export const fetchCharacters = () => {
   const data = fetch(`https://rickandmortyapi.com/api/character`)
     .then((data) => data.json())
     .then((charactersData) => {
@@ -8,11 +8,27 @@ export const getCharacters = () => {
 };
 
 //  TODO: write a fetch request using async/await (log both results)
-// export async function getCharacters() {
-//   const response = await fetch(`https://rickandmortyapi.com/api/character`);
-//   const charactersData = await response.json();
-//   return charactersData;
-// }
+export async function getCharacters() {
+  const response = await fetch(`https://rickandmortyapi.com/api/character`);
+  const charactersData = await response.json();
+  return charactersData;
+}
+// more practice
+export async function getStatus() {
+  const response = await fetch(`https://rickandmortyapi.com/api/character`);
+  const charactersData = await response.json();
+  const charactersList = charactersData.results;
+  console.log(charactersData);
+  return charactersList.map((item) => item.status);
+}
+
+export const fetchStatus = () => {
+  const data = fetch(`https://rickandmortyapi.com/api/character`)
+    .then((data) => data.json())
+    .then((charactersData) => {
+      console.log(charactersData);
+    });
+};
 
 /**
  * TODO:

ef72bfd9c79d9bd4c83415beaff3c224e12da6d9
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-25 11:28:08 -0800

    got both async/await and .then to work with api

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import { getCharacters } from './services/promise-me';
+import { useEffect } from 'react';
+
 export default function App() {
+  useEffect(() => {
+    getCharacters();
+  });
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,10 +1,18 @@
-/**
- * TODO:
- */
+//   TODO: write a fetch request to an api url using .then()
+export const getCharacters = () => {
+  const data = fetch(`https://rickandmortyapi.com/api/character`)
+    .then((data) => data.json())
+    .then((charactersData) => {
+      console.log(charactersData);
+    });
+};
 
-/**
- * TODO:
- */
+//  TODO: write a fetch request using async/await (log both results)
+// export async function getCharacters() {
+//   const response = await fetch(`https://rickandmortyapi.com/api/character`);
+//   const charactersData = await response.json();
+//   return charactersData;
+// }
 
 /**
  * TODO:
